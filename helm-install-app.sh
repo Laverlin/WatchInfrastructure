247b72b996e79757238eb3b07785829a5dcda6a4
Laverlin <ilaverlin@gmail.com>
2023-04-18 21:10:49 +0200

    add some home stuff

--- helm-install-app.sh
+++ helm-install-app.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cd helm
-helm upgrade watch-micro watch-micro -n watch-backend --install --values prod-values.secret.yaml --kubeconfig ~/remote-kube/ivan-b.com/config
+helm upgrade watch-micro watch-micro -n watch-backend --install --values secret.prod-values.yaml --kubeconfig ~/remote-kube/ivan-b.com/config
 cd ..
 
 
